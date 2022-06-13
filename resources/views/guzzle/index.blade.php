cf265775f95cd7bf5bebf993cf72a9aa6d9cb1cc
ray95609 <ray95609@gmail.com>
2022-06-13 23:56:36 +0800

    20220613-2356-美化跟存入資料庫再寫出-循環列印出七天

--- resources/views/guzzle/index.blade.php
+++ resources/views/guzzle/index.blade.php
@@ -5,20 +5,32 @@
 @section('content')
 
 
-    <!--用日期的套件算出差距日-->
-
-
-    <h5>COVID-19 確診數據</h5>
-    <p>日期:{{$onede[$puls]['a04']}}</p>
-    <p>國家:{{$onede[$puls]['a03']}}</p>
-    <p>地區:{{$onede[$puls]['a02']}}</p>
-    <p>總確診:{{$onede[$puls]['a05']}}</p>
-    <p>新增確診:{{$onede[$puls]['a07']}}</p>
-
-
     <!--接下來要存入資料庫  再讀出來  稍微研究一下boostrap-->
+    <!--迴圈列印出7天-->
+    <table class="table table-hover">
+        <thead class="thead-dark">
 
+        <label class="row justify-content-center"><strong>COVID-19 確診數據</strong></label>
+        <tr>
+            <th>日期</th>
+            <th>國家</th>
+            <th>地區</th>
+            <th>總確診</th>
+            <th>新增確診</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>{{$onede[$puls]['a04']}}</td>
+            <td>{{$onede[$puls]['a03']}}</td>
+            <td>{{$onede[$puls]['a02']}}</td>
+            <td>{{$onede[$puls]['a05']}}</td>
+            <td>{{$onede[$puls]['a07']}}</td>
+        </tr>
 
+        </tbody>
+
+    </table>
 
 
 @endsection

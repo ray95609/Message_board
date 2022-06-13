aa54a60f60b43c709a7e1da65e8ce0c1322b0667
ray95609 <ray95609@gmail.com>
2022-06-13 23:11:49 +0800

    part_complete

--- app/Http/Controllers/GuzzleController.php
+++ app/Http/Controllers/GuzzleController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
-class GuzzleController
+use GuzzleHttp\Client;
+
+class GuzzleController extends Controller
 {
+
     public function index(){
 
+        $client = new Client();
+        $response = $client->get('https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=3001&limited=BGD
+');
+        $deresponse= json_decode( $response->getBody()->getContents(), true);
 
-        
-        return view('guzzle.index');
+        $onedate=$deresponse;
+
+        $onede=$onedate;
+
+
+        return view('guzzle.index',['onede'=>$onede]);
     }
-
 }

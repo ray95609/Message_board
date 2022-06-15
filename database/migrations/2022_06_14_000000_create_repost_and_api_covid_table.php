77bf28bfa51de8d3ff7740dbdc5ebebe4b8c8330
rogerlu <rogerlu@AHHQ.ANDENHUD.COM>
2022-06-15 11:59:59 +0800

    修正db migrate 檔案錯誤

--- database/migrations/2022_06_14_000000_create_repost_and_api_covid_table.php
+++ database/migrations/2022_06_14_000000_create_repost_and_api_covid_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreateRepostAndApiCovidTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,19 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('post_name',255)->comment('文章名稱');
-            $table->text('post_content')->comment('文章內容');
-            $table->integer('post_user_id')->comment('文章發文人');
-            //設定 創建文章時   自動帶入 當時時間 CURRENT_TIMESTAMP
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'))->comment('創建時間');
-            //設定 當更新文章時 自動帶入  當時時間 CURRENT_TIMESTAMP
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))->comment('文章更新時間');
-        });
-        //對表註解
-        \DB::statement("ALTER TABLE `posts` comment '文章'");
+        /** 另外一個檔案有了 **/
+//        Schema::create('posts', function (Blueprint $table) {
+//            $table->bigIncrements('id');
+//            $table->string('post_name',255)->comment('文章名稱');
+//            $table->text('post_content')->comment('文章內容');
+//            $table->integer('post_user_id')->comment('文章發文人');
+//            //設定 創建文章時   自動帶入 當時時間 CURRENT_TIMESTAMP
+//            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'))->comment('創建時間');
+//            //設定 當更新文章時 自動帶入  當時時間 CURRENT_TIMESTAMP
+//            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))->comment('文章更新時間');
+//        });
+//        //對表註解
+//        \DB::statement("ALTER TABLE `posts` comment '文章'");
 
 
         Schema::create('repost',function (Blueprint $table){
@@ -33,10 +34,10 @@ class CreatePostsTable extends Migration
             $table->string('repost_name');
             $table->text('repost_content');
             $table->bigInteger('repost_user_id');
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));;
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));;
         });
-        \DB::statement("ALTER TABLE 'repost' comment '回覆文章'");
+        \DB::statement("ALTER TABLE `repost` comment '回覆文章'");
 
         Schema::create('api_covid_19',function (Blueprint $table){
             $table->bigIncrements('id');
@@ -49,7 +50,7 @@ class CreatePostsTable extends Migration
             $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
 
         });
-
+        \DB::statement("ALTER TABLE `api_covid_19` comment 'covid_19 API 測試'");
     }
 
 
@@ -60,6 +61,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        //記得要做 當有人rollback 才會正確刪除
+        Schema::dropIfExists('repost');
+        Schema::dropIfExists('api_covid_19');
     }
 }
